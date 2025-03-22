9d24f96f473c6abaa598e44bd39d538cedfb7031
Adam Montecarlo <gtimarakov952@gmail.com>
2025-03-22 17:11:37 +0200

    p

---
+++ WebApplication3/Controllers/FavoritesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.DBcontext.WebCinema.Data;
+using WebApplication3.Models;
+using WebApplication3.Extensions;
+using System.Collections.Generic;
+
+namespace WebApplication3.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var favoriteFilms = HttpContext.Session.GetObjectFromJson<List<Film>>("FavoriteFilms") ?? new List<Film>();
+            return View(favoriteFilms);
+        }
+
+        public IActionResult AddToFavorites(int id)
+        {
+            var film = _context.Films.Find(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            var favoriteFilms = HttpContext.Session.GetObjectFromJson<List<Film>>("FavoriteFilms") ?? new List<Film>();
+            favoriteFilms.Add(film);
+            HttpContext.Session.SetObjectAsJson("FavoriteFilms", favoriteFilms);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult RemoveFromFavorites(int id)
+        {
+            var favoriteFilms = HttpContext.Session.GetObjectFromJson<List<Film>>("FavoriteFilms") ?? new List<Film>();
+            var film = favoriteFilms.Find(f => f.Id == id);
+            if (film != null)
+            {
+                favoriteFilms.Remove(film);
+                HttpContext.Session.SetObjectAsJson("FavoriteFilms", favoriteFilms);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -10,19 +10,23 @@ namespace WebApplication3
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            builder.Services.AddLogging();
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
@@ -34,16 +38,13 @@ namespace WebApplication3
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSession();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
-
-
-
         }
-
     }
 }
